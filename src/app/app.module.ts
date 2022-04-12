b98443972706d56a687be8de5a4015b8abc4a92e
Clement Carcenac <clementcarcenac@gmail.com>
2022-04-12 16:33:54 +0200

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FolioComponent } from './folio/folio.component';
+import { ContactComponent } from './contact/contact.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FolioComponent,
+    ContactComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule
