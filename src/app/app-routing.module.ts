02d0b4826c406adcfaf9250ceb2fee85e973a6c6
Clement Carcenac <clementcarcenac@gmail.com>
2022-05-04 16:42:11 +0200

    sk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CvComponent } from "./cv/cv.component";
+import { FilesComponent } from "./files/files.component";
 import { HomeComponent } from "./home/home.component";
+import { SkillComponent } from "./skill/skill.component";
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent},
-    // { path: '/'}
+    { path: 'cv', component: FilesComponent, children: [
+        {
+            path: 'html', component: CvComponent
+        },
+        {
+            path: 'css', component: SkillComponent
+        }
+    ]}
 ]
 
 @NgModule({
