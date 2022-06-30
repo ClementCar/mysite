f06429959b580f7f4c0ee733618ab3c6ed9e1eda
Clement Carcenac <clementcarcenac@gmail.com>
2022-06-30 16:35:41 +0200

    anim mod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,40 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
+// import {
+//   trigger,
+//   state,
+//   style,
+//   animate,
+//   transition
+// } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  // animations: 
+  // animations: [
+  //   trigger('changeDivDimension', [
+  //     state('initial', style({
+  //       backgroundColor : 'yellow',
+  //       height : '100px',
+  //       witdh : '100px'
+  //     })),
+  //     state('final', style({
+  //       backgroundColor : 'blue',
+  //       height : '200px',
+  //       width : '200px'
+  //     })),
+  //     transition('initial=>final', animate('1000ms')),
+  //     transition('final=>initial', animate('2000ms'))
+  //   ])
+  // ]
 })
 export class AppComponent implements OnInit{
   title = 'mysite';
   theme: Theme = 'dark-theme';
+  // 
+  stateAct = 'initial';
 
   constructor(
     private router: Router,
@@ -40,6 +57,11 @@ export class AppComponent implements OnInit{
   //   if (window.matchMedia('(prefers-color-scheme: light)').matches) {
   //     this.renderer.addClass(this.document, this.theme);
   //   } 
+  // }
+
+  // 
+  // changeState() {
+  //   this.stateAct = this.stateAct === 'initial' ? 'final' : 'initial';
   // }
 
 }
