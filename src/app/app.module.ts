ec011e54d811104c9d27a390203fed373662ed0d
Clement Carcenac <clementcarcenac@gmail.com>
2022-11-03 14:38:05 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-Browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as fr from '@angular/common/locales/fr';
 
 import 'hammerjs';
