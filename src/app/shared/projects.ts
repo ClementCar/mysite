0ee842f5aac4690e7371a1aae7482e6a85124908
Clement Carcenac <clementcarcenac@gmail.com>
2022-11-07 17:00:11 +0100

    All Project

--- src/app/shared/projects.ts
+++ src/app/shared/projects.ts
@@ -3,17 +3,41 @@ import { Project } from "./project";
 export const PROJECTS: Project[] = [
     {
         id: '0',
-        name: 'Oh My Food',
-        image: '../../assets/myfood/Accueil.JPG',
-        video: '/assets/myfood/Food-tel.mp4',
-        description: "Dynamisation d'une page Web",
-        gitlink: 'https://github.com/ClementCar/ClementCarcenac_3_06122021.git'
+        name: 'Groupomania',
+        image: '../../assets/Groupomania/Log.JPG',
+        video: '../../assets/Groupomania/Groupomania.mp4',
+        description: "Création d'un réseau social d'Entreprise",
+        gitlink: 'https://github.com/ClementCar/Groupomania.git'
     },
     {
         id: '1',
+        name: 'Piiquante',
+        image: '../../assets/piiquante/log.JPG',
+        video: '../../assets/piiquante/piiquante.mp4',
+        description: "Construction d'une API",
+        gitlink: 'https://github.com/ClementCar/Piiquante.git'
+    },
+    {
+        id: '2',
+        name: 'Kanap',
+        image: '../../assets/kanap/home.JPG',
+        video: '../../assets/kanap/Kanap.mp4',
+        description: "Liaison Front-end / Back-end",
+        gitlink: 'https://github.com/ClementCar/Kanap_5_12-01-22.git'
+    },
+    {
+        id: '3',
+        name: 'Chouette Agence',
+        image: '../../assets/chouette/Home1.JPG',
+        video: '../../assets/chouette/Chouette.mp4',
+        description: "Optimisation SEO d'un site",
+        gitlink: 'https://github.com/ClementCar/Projet-4.git'
+    },
+    {
+        id: '4',
         name: 'Oh My Food',
         image: '../../assets/myfood/Accueil.JPG',
-        video: '/assets/myfood/Food-tel.mp4',
+        video: '../../assets/myfood/Food-tel.mp4',
         description: "Dynamisation d'une page Web",
         gitlink: 'https://github.com/ClementCar/ClementCarcenac_3_06122021.git'
     }
